b461905257523f4f3afd1ab2cd529da470fce2df
PradoMarcelo777 <geotzmprado777@outlook.com>
2024-10-30 17:31:10 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace gpradoT5A
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Views.Principal();
+        }
+    }
+}
